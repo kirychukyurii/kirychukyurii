654201c2b87f927ac1182fec8090ffe65e0e4d27
kirychukyurii <yurii.kirychuk.knz.2017@lpnu.ua>
2021-04-19 09:09:44 +0300

    nothing

--- http.sh
+++ http.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-nc -lp $HTTP_PORT < /app/index.html
-
+while true;
+do echo -e "HTTP/1.1 200 OK\r\n$(date)\r\nContent-Type: text/html; charset=utf-8\r\n\r\n" "$(cat /app/index.html)" | nc -lp $HTTP_PORT;
+done
